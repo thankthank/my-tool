03eb76e6fc858c33b9559c15f05241bada6f1ea8
thankthank <chris.chon@suse.com>
2019-09-03 19:14:16 +0900

    Fix bug

--- obs.sh
+++ obs.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+##Version setting
 osc checkout home:cchon my-tool
 VER_MAJOR=$(ls home:cchon/my-tool | grep tar.gz | awk -F\- '{print $3}' | awk -F. '{print $1}')
 CUR_VER_MINOR=$(ls home:cchon/my-tool | grep tar.gz | awk -F\- '{print $3}' | awk -F. '{print $2}')
 (( NXT_VER_MINOR=CUR_VER_MINOR + 1 ))
 VERSION=${VER_MAJOR}.${NXT_VER_MINOR}
 VERSION_CUR=${VER_MAJOR}.${CUR_VER_MINOR}
-
 rm -rf home:cchon
 
+
+
+##Creating of source file as tar.gz with files from github 
 cd ..
 #current directory : {parent directory of local git repo}
 
@@ -19,16 +22,19 @@ mv my-tool-${VERSION}.tar.gz my-tool-$VERSION/
 cd my-tool-${VERSION}
 #current directory : {parent directory..}/my-tool-${VERSION}
 
+
+
+## Packaging and commit my-tool to obs
 osc checkout home:cchon my-tool
 
 cd home\:cchon/my-tool/
 #current directory :  {parent directory of local git repo}/my-tool-${VERSION}/home:cchon/my-tool
 
-##package setup process
+#package setup process
 osc delete my-tool*.tar.gz
 
-cp ../../my-tool-${VERSION}.tar.gz .
-sed -i "s/Version: ${VERSION_CUR}/Version: ${VERSION}/g" my-tool.spec
+mv ../../my-tool-${VERSION}.tar.gz .
+sed -i "s/Version:\t${VERSION_CUR}/Version:\t${VERSION}/g" my-tool.spec
 
 osc vc
 
