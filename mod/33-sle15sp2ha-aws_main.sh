db73de390ffc87046dc6f08cde95cd1e6693904c
thankthank <chris.chon@suse.com>
2021-01-12 23:22:39 +0900

    ha-aws updated

--- mod/33-sle15sp2ha-aws_main.sh
+++ mod/33-sle15sp2ha-aws_main.sh
@@ -12,13 +12,22 @@
 #ConfigureNetwork on_GRP0
 
 ## Basic Cluster with fence agent and overlay IP
-#Corosynckey on_GRP2
-#BootstrapCluster on_GRP-1
-#ConfigureProperty_Stonith on_GRP2
-
+#Corosynckey on_GRP3
+#BootstrapCluster on_GRP0
+#ConfigureProperty_Stonith on_GRP3
 
 ## Install SAP applications
 
 ## HA for SAP HANA
 #sapHANA_HA_DR_profider on_GRP0
+#sapHANA_crm_configuration on_GRP3
+
+# HANA optional configuration : Read enabled on secondary HANA.
+#sapHANA_crm_read-enabled on_GRP3
+
+## HA for SAP ASCS-ERS
+
+
+
+#HA_test on_GRP3
 
